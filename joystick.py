6f79f7be4b213ca0d95f9db2bacaedb0c588a1b8
NoobVrGT <unspeakableliam1@gmail.com>
2026-01-11 12:49:22 -0500

    Add initial Joystick class with placeholder methods

---
+++ joystick.py
@@ -0,0 +1,11 @@
+# joystick.py
+# Handles KEY1/2/3 and joystick directions
+
+class Joystick:
+    def __init__(self):
+        # TODO: set up GPIO pins for buttons and joystick
+        pass
+
+    def read(self):
+        # TODO: return which button or direction is pressed
+        return None
