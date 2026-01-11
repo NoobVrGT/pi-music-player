a65bac6f1962a7287d63c7462dab500485996153
NoobVrGT <unspeakableliam1@gmail.com>
2026-01-11 12:49:39 -0500

    Add initial UI class for user interface handling

---
+++ ui.py
@@ -0,0 +1,15 @@
+# ui.py
+# Draws the user interface (album art, text, menus, etc.)
+
+class UI:
+    def __init__(self):
+        # TODO: load fonts, colors, layout settings
+        pass
+
+    def draw_now_playing(self, lcd, song_info):
+        # TODO: draw album art, title, artist, progress bar
+        pass
+
+    def draw_menu(self, lcd, items, selected_index):
+        # TODO: draw a simple menu
+        pass
