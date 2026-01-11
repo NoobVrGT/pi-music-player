47b3468f004506bc8d6dc7899278be162ec1295f
NoobVrGT <unspeakableliam1@gmail.com>
2026-01-11 12:49:56 -0500

    Add AudioPlayer class for music playback
    Initial implementation of AudioPlayer class with methods for playback control and metadata retrieval.

---
+++ Audio.py
@@ -0,0 +1,23 @@
+# audio.py
+# Handles music playback and metadata
+
+class AudioPlayer:
+    def __init__(self):
+        # TODO: initialize audio backend
+        pass
+
+    def play(self, filepath):
+        # TODO: play a song
+        pass
+
+    def pause(self):
+        # TODO: pause playback
+        pass
+
+    def next_track(self):
+        # TODO: skip to next track
+        pass
+
+    def get_metadata(self, filepath):
+        # TODO: return title, artist, album art
+        return {}
