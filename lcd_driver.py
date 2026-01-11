fee42f24466123d97f67c6974b9fb8b6a0434522
NoobVrGT <unspeakableliam1@gmail.com>
2026-01-11 12:48:57 -0500

    Create lcd_driver.py for LCD HAT control

---
+++ lcd_driver.py
@@ -0,0 +1,15 @@
+# lcd_driver.py
+# Handles SPI communication and drawing to the 1.3" LCD HAT
+
+class LCD:
+    def __init__(self):
+        # TODO: initialize SPI, GPIO, and the ST7789 display
+        pass
+
+    def clear(self, color=(0, 0, 0)):
+        # TODO: fill the screen with a solid color
+        pass
+
+    def draw_image(self, image):
+        # TODO: send a PIL image to the display
+        pass
